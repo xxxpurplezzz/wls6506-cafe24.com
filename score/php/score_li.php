6f1c854a764bf39067ec854c31b22e0f43860211
xxxpurplezzz <yojins17@naver.com>
2019-11-21 17:49:30 +0900

    port

---
+++ score/php/score_li.php
@@ -0,0 +1,33 @@
+<?
+include '../inc/connect.php';
+if(isset($_GET['page'])) {
+	$page = $_GET['page'];
+}
+else $page = 1;
+$std = ($page - 1) * 5;
+$len = 5;
+
+$sql = " SELECT count(id) FROM score";
+$result = mysqli_query($conn, $sql);
+$rs = mysqli_fetch_array($result);
+$total = $rs[0];
+
+$sql = " SELECT * FROM score ORDER BY id DESC LIMIT $std, $len";
+$result = mysqli_query($conn, $sql);
+
+
+$str = '{';
+$str.= 	'"total": '.$total.', "student" : [ ';
+while($rs = mysqli_fetch_array($result)) {
+	$str.= '{';
+	$str.= ' "id": "'.$rs['id'].'", ';
+	$str.= ' "stdname": "'.$rs['stdname'].'", ';
+	$str.= ' "kor": "'.$rs['kor'].'", ';
+	$str.= ' "eng": "'.$rs['eng'].'", ';
+	$str.= ' "math": "'.$rs['math'].'" ';
+	$str.= '},';
+}
+$str = substr($str, 0, -1);
+$str.= ' ] }';
+echo $str;
+?>
